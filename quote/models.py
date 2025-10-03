7078dfe9afe68a904884775d43135bd092efebf6
Oleksii Klymenok <oleksii@nix-pro.com>
2025-10-03 00:37:43 -0700

    Fix text field in the model

--- quote/models.py
+++ quote/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class WisdomQuote(models.Model):
-  quote_text = models.TextField
+  quote_text = models.TextField(default='')
   author = models.CharField(max_length=50)
   reference = models.CharField(max_length=50)
